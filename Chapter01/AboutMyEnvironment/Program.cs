4e432bd291a632f4bebc11d78c8c1d718092a2ac
RobertGheo <robert.bradu@yahoo.com>
2023-06-07 13:25:31 +0300

    upload chapter 01

---
+++ Chapter01/AboutMyEnvironment/Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, this is my environment");
+Console.WriteLine(">>>>> {0}", Environment.CurrentDirectory);
+Console.WriteLine(">>>>> {0}", Environment.OSVersion.VersionString);
+Console.ReadKey();
